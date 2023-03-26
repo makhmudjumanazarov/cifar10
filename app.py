03619690b6f819184b7417f83ab1f11bc10567d2
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-26 17:40:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,12 @@ model_load = load_model('model')
 
 st.title('CIFAR10 Image Recognizer')
 labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
-# st.header(":white[Sample images for classes]")
 
-# clas = st.radio(
-# "Choose class",
-# classes, horizontal=True)
-# images = get_images(option, clas)
-# rand = randint(0, 9)
-# a = cv2.resize(images[rand], (128,128), interpolation = cv2.INTER_AREA)
-# st.image(a)
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if img_file_buffer is not None:
     image = Image.open(img_file_buffer)
     img_array = np.array(image)
-
-
+    
 if st.button('Predict'):
     try:
         img_array = cv2.resize(img_array.astype('uint8'), (32, 32))
@@ -35,6 +26,5 @@ if st.button('Predict'):
         img_array = img_array.transpose((1,0,2,3))
         val = model_load.predict(img_array)
         st.write(f'result: {labels[np.argmax(val[0])]}')
-#         st.bar_chart(val[0])
     except:
         pass
