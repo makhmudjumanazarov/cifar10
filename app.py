4261fc711d80f035bab08524d3d8a4690d815baa
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-27 01:19:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ import cv2
 from PIL import Image
 from svgpathtools import parse_path
 from pathlib import Path
+from glob import glob
 from tensorflow.keras.models import load_model
+
+def get_images(data, clas):
+    return [cv2.imread(i) for i in glob('images/'+data+'/'+clas+'*')]
 
 model_load = load_model('model')
 
 st.title('CIFAR10 Image Recognizer')
+clas = st.radio(
+"Choose class",
+classes, horizontal=True)
+images = get_images(option, clas)
+rand = randint(0, 9)
+a = cv2.resize(images[rand], (128,128), interpolation = cv2.INTER_AREA)
+st.image(a)
 labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
