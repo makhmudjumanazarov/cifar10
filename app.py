68827ce2e71da188cc68f52b199a8758405c74f8
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-04-13 15:06:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,24 +12,25 @@ from tensorflow.keras.models import load_model
 
 
 model_load = load_model('model')
+st.write(model_load.summary())
 
-st.title('CIFAR10 Image Recognizer')
-labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+# st.title('CIFAR10 Image Recognizer')
+# labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 
 
-img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-if img_file_buffer is not None:
-    image = Image.open(img_file_buffer)
-    img_array = np.array(image)
+# img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+# if img_file_buffer is not None:
+#     image = Image.open(img_file_buffer)
+#     img_array = np.array(image)
     
-if st.button('Predict'):
-    try:
-        img_array = cv2.resize(img_array.astype('uint8'), (32, 32))
-        img_array = np.expand_dims(img_array, axis=1)
-        img_array = img_array.transpose((1,0,2,3))
-        val = model_load.predict(img_array)
-        output_text = labels[np.argmax(val[0])]
-        font_size = "24px"
-        st.markdown("<h4 style='text-align: left; color: #2F3130; font-size: {};'>{}</h4>".format(font_size, output_text), unsafe_allow_html=True)
-    except:
-        pass
+# if st.button('Predict'):
+#     try:
+#         img_array = cv2.resize(img_array.astype('uint8'), (32, 32))
+#         img_array = np.expand_dims(img_array, axis=1)
+#         img_array = img_array.transpose((1,0,2,3))
+#         val = model_load.predict(img_array)
+#         output_text = labels[np.argmax(val[0])]
+#         font_size = "24px"
+#         st.markdown("<h4 style='text-align: left; color: #2F3130; font-size: {};'>{}</h4>".format(font_size, output_text), unsafe_allow_html=True)
+#     except:
+#         pass
